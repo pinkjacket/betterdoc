d96301fd1fa681c6e3befc686d0c1d52039be452
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-02-19 23:37:14 -0800

    name search now returns list of doctors with all info, moved some things around so it looks better

--- src/backend.js
+++ src/backend.js
@@ -11,45 +11,19 @@ export function nameSearch(name) {
     },
     success: function(response) {
       if(response.data.length === 0){
-        $(".showErrors").text("No doctors found, sorry.")
+        $(".noDoctors").text("No doctors found, sorry.")
       }
       for(let i = 0; i < response.data.length; i++) {
         let name = response.data[i].profile.first_name + " " + response.data[i].profile.last_name;
         let address1 = response.data[i].practices[0].visit_address.street;
-        let address2 = response.data[i].practices[0].visit_address.street2;
-        let address3 = response.data[i].practices[0].visit_address.zip;
-        $(".showDoctors").append("<li>Name: " + name + "</li>" + "<p></p>" + "Address: " + address1 + "<p></p>" + address2 + "<p></p>" + address3);
+        let address2 = response.data[i].practices[0].visit_address.zip;
+        let phone = response.data[i].practices[0].phones[0].number;
+        let newpatients = response.data[i].practices[0].accepts_new_patients;
+        $(".showDoctors").append("<ul>" + "<li>Name: " + name + "</li>" + "<p></p>" + "Address: " + address1 + "<p></p>" + address2 + "<p></p>" + "Phone#: " + phone + "<p></p>" + "Open for new patients? " + newpatients + "</ul>");
       }
     },
-
-    //   })
-    //
-    // },
     error:function() {
       $('.showErrors').text("Error!")
     }
   });
 }
-  // let promise = new Promise(function(resolve, reject) {
-    // let request = new XMLHttpRequest();
-    // let url = term;
-    // request.open("GET", url, true);
-    // request.onload = function() {
-    //   let data = JSON.parse(this.response);
-    //
-    //   if (this.status >= 200 && this.status < 400) {
-    //     data.forEach()
-    //   } else {
-    //     reject(Error(request.statusText));
-    //   }
-    // }
-    // request.send();
-  // });
-
-//   promise.then(function(response) {
-//     let body = JSON.parse(response);
-//     $(".showDoctors").text(`Doctor's name: ${body.data.profile.last_name}`);
-//   }, function(error) {
-//     $(".showErrors").text(`There was an error processing your request: ${error.message}`);
-//   });
-// };
