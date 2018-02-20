868e51648a8d5a8cafc426c9949bcf287b8f6038
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-02-19 22:29:24 -0800

    api call now actually working, took out a whole bunch of code I realized I didn't actually need

--- src/backend.js
+++ src/backend.js
@@ -2,25 +2,41 @@ var apiKey = require("./../.env").apiKey;
 
 
 
-export function nameSearch(term) {
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = term;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
+export function nameSearch(name) {
+  $.ajax({
+    url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(response) {
+      console.log(`${response.data[0].profile.first_name}`);
+    },
+    error:function() {
+      $('.showErrors').text("Error!")
     }
-    request.open("GET", url, true);
-    request.send();
   });
+}
+  // let promise = new Promise(function(resolve, reject) {
+    // let request = new XMLHttpRequest();
+    // let url = term;
+    // request.open("GET", url, true);
+    // request.onload = function() {
+    //   let data = JSON.parse(this.response);
+    //
+    //   if (this.status >= 200 && this.status < 400) {
+    //     data.forEach()
+    //   } else {
+    //     reject(Error(request.statusText));
+    //   }
+    // }
+    // request.send();
+  // });
 
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    $(".showDoctors").text(`Doctor's name: ${body.data.profile.last_name}`);
-  }, function(error) {
-    $(".showErrors").text(`There was an error processing your request: ${error.message}`);
-  });
-};
+//   promise.then(function(response) {
+//     let body = JSON.parse(response);
+//     $(".showDoctors").text(`Doctor's name: ${body.data.profile.last_name}`);
+//   }, function(error) {
+//     $(".showErrors").text(`There was an error processing your request: ${error.message}`);
+//   });
+// };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ var apiKey = require("./../.env").apiKey;
 import { nameSearch } from "./backend.js";
 
 $(document).ready(function() {
-  $('#docName').click(function() {
+  $('#nameSearch').click(function() {
     let name = $('#docName').val();
     $('#docName').val("");
+    let response = nameSearch(name);
 
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
-    let response = nameSearch(url);
-    
 
   });
 });
